3dbbdc6c3f90746bd5673ac0f3933b7aa80adf5e
Sumit Toshniwal <sumit@mindpathtech.com>
2019-09-27 13:45:02 +0530

    Add and delete functionality added

--- angular-node-express-api/routes/heroes.js
+++ angular-node-express-api/routes/heroes.js
@@ -17,6 +17,23 @@ router.get("/:id", async (req, res) => {
     let heroId = req.params.id;
     let hero = await HeroService.getHeroById(heroId);
     res.json(hero);
-})
+});
+
+router.put("/:id", async (req, res) => {
+    let heroId = req.params.id;
+    let hero = await HeroService.updateHeroDetails(heroId, req.body);
+    res.json(hero);
+});
+
+router.post("/", async (req,res) => {
+    let newHero = await HeroService.addHero(req.body);
+    res.json(newHero);
+});
+
+router.delete("/:id", async (req, res) => {
+    let heroId = req.params.id;
+    await HeroService.deleteHero(heroId);
+    res.json();
+});
 
 module.exports = router;
--- angular-node-express-api/services/hero.service.js
+++ angular-node-express-api/services/hero.service.js
@@ -1,4 +1,4 @@
-const heroes = [
+var heroes = [
     { id: 11, name: 'Dr Nice' },
     { id: 12, name: 'Narco' },
     { id: 13, name: 'Bombasto' },
@@ -18,5 +18,31 @@ module.exports = class HeroService {
     }
     static getHeroById(id) {
         return heroes.find(hero => hero.id == id)
+    }
+    static updateHeroDetails(id, hero) {
+        let oldHero = HeroService.getHeroById(id);
+        oldHero.name = hero.name;
+        return oldHero;
+    }
+
+    static addHero(hero) {
+        let maxId = 0;
+        heroes.find(hero => {
+            if(hero.id >= maxId) {
+                maxId = hero.id
+            }
+        });
+        let newHero = {
+            id : maxId + 1,
+            name : hero.name
+        }
+        heroes.push(newHero);
+        return newHero;
+    }
+
+    static deleteHero(heroId) {
+        heroes = heroes.filter((hero) => {
+            return hero.id != heroId;
+        });
     }
 }
